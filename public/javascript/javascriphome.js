0576234dc322cd5fc7f9d12ebcfc774b50e54692
Warolf <jesusrdiogo@gmail.com>
2018-07-08 23:38:50 +0100

    Update javascriphome.js

--- public/javascript/javascriphome.js
+++ public/javascript/javascriphome.js
@@ -2,6 +2,7 @@ var top = `
 <div class="row">
               <div class="col-sm-11">
                 <img src="" />
+<h1 id="welcomeTitle">Welcome to Vigil</h1>
               </div>
               <div class="col-sm-1">
                     {{#names}}
