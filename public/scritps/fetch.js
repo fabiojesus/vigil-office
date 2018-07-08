ce1d28d9cf197bca9c1e40c873dbe6318477fa48
PedroCost <pedroc_27@hotmail.com>
2018-07-08 22:35:54 +0100

    Fetch

---
+++ public/scritps/fetch.js
@@ -0,0 +1,36 @@
+const URL = 'https://vigil-server.herokuapp.com/?query='
+
+function fetchGet(url , cb) {
+    return fetch(URL + escapeHtml(url))
+        .then(res => res.json())
+        .then(res => { return res })
+        .catch(err => { return err; })
+}
+
+function fetchPost(url ,cb) {
+    return fetch(URL + escapeHtml(url), {
+        method: 'POST'
+    })
+        .then(res => res.json())
+        .then(res => { return res })
+        .catch(err => { return err; })
+}
+
+
+function escapeHtml(text) {
+    var map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        "'": '&#039;',
+        ",": '%2C',
+        "{": '%7B',
+        "}": '%7D',
+        "@": '%40',
+        ":": '%3A',
+        "(": '(',
+        ")": ')',
+        '"': '"',
+    };
+    return text.replace(/[&<>"',{}@:]()/g, function (m) { return map[m]; });
+}
