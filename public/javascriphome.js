c2f6d5e94ffaf3bcd7f00f78a6fb43c3c8edb1cf
Warolf <jesusrdiogo@gmail.com>
2018-07-08 23:09:32 +0100

    Create javascriphome.js

---
+++ public/javascriphome.js
@@ -0,0 +1,12 @@
+var top = `
+<div class="row">
+              <div class="col-sm-11">
+                <img src="" />
+              </div>
+              <div class="col-sm-1">
+                    {{#names}}
+                     <li>{{name}}</li>
+                    {{/names}}
+                    <button>Logout</button>
+                </div>
+            </div>`;
